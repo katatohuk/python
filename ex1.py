5c303ffa722f845fa24ea8c761d64d0a876ce00e
Katatohuk <katatohuk@gmail.com>
2016-11-08 16:28:03 +0200

    aa

---
+++ ex1.py
@@ -0,0 +1,6 @@
+print "Hello world"
+print "Heello again"
+print "I like typing this"
+print "This is fun :)"
+print "yaa,Printing"
+
---
+++ ex5.py
@@ -0,0 +1,18 @@
+my_name = "Andrii"
+my_age = 31
+my_height = 193
+my_height_in = my_height * 0.39370
+my_weight = 93
+my_weight_lb = my_weight * 2.2046
+my_eyes = 'blue'
+my_teeth = 'white'
+my_hair = 'blond'
+
+print "Lets talk about %s" % my_name
+print "He's %d santimeter (%d inches) tall" % (my_height, my_height_in)
+print "He's %d kgs (%d lbs) heavy" % (my_weight, my_weight_lb)
+print "Actually thtat's not too heavy :)"
+print "He's got %s eyes and %s hair" % (my_eyes, my_hair)
+print "His teeth are usually %s depenging on coffee" % my_teeth
+
+print "If I add %d, %d, and %d I get %d" % (my_age, my_height, my_weight, my_age + my_height + my_weight)
