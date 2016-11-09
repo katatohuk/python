f6e9a410e36155b9529b65860a0a031955dfe59e
Katatohuk <katatohuk@gmail.com>
2016-11-09 16:32:59 +0200

    ex10.py added

---
+++ ex10.py
@@ -0,0 +1,10 @@
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm splint \non a line."
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fish
+\t* Catnip\n\t* Grass
+"""
