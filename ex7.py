d8d2d9053ee544f14a615b03bec553c8734c4508
Katatohuk <katatohuk@gmail.com>
2016-11-09 10:49:51 +0200

    sss

---
+++ ex7.py
@@ -0,0 +1,19 @@
+print "Mary havd a little lamb"
+print "Its fleece was white as %s" % 'snow'
+print "And everywhere that Mary went"
+print "." * 10
+
+
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "s"
+end5 = "B"
+end6 = "u"
+end7 = "r"
+end8 = "g"
+
+print end1 + end2 + end3 * 2 + end4 + end3,
+print end5 + end6 + end7 + end8 + end3 + end7
+
+
