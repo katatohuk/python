2176ed0e2dbbcf54be8918d7495a06c5b6b70abc
Katatohuk <katatohuk@gmail.com>
2016-11-09 15:30:02 +0200

    sssss

--- ex9.py
+++ ex9.py
@@ -1,2 +1,12 @@
-ssss
-adasdasd
+days = "Mon Tue Wed Thi Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec"
+#print months
+#print days
+print "Here are days: ", days
+print "Here are months: ", months
+print """
+		There is something going on here.
+		With the three doulbe quotes.
+		We'll be albe to type as much as we like.
+		Even 4 lines if we want, or 5, or 6.
+		§"""
