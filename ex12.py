05f550195d027d250ac93ca0d923a03b8378ed56
Katatohuk <katatohuk@gmail.com>
2016-11-10 15:18:25 +0200

    s

---
+++ ex12.py
@@ -0,0 +1,5 @@
+age = raw_input("How old are you? ")
+height = raw_input("How tall are you? ")
+weight = raw_input("How much do u weight? ")
+
+print "So u'r %s years old, %s tall and %s " % (age, height, weight)
