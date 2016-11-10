f4609b1eb47f1e7afa34a207b2ae6de4b4c24cfd
Katatohuk <katatohuk@gmail.com>
2016-11-10 12:53:41 +0200

    ex11_1.py added

--- ex11.py
+++ ex11.py
@@ -1,8 +1,8 @@
 print "How old are you?",
-age = raw_input()
+age = raw_input("Please specify your age: ")
 print "How tall are you?",
 height = raw_input()
 print "How much do you weight?",
 weight = raw_input()
 
-print "So you're %r years old, %r tall and %r weight" % (age, height, weight)
+print "So you're %s years old, %s tall and %s weight" % (age, height, weight)
---
+++ ex11_1.py
@@ -0,0 +1,22 @@
+#lets try some choise menu
+from datetime import datetime
+
+print 30 * '-'
+print " MAIN MENU"
+print 30 * '-'
+print "1. Show me current date"
+print "2. Start backup"
+print "3. Exit programm"
+
+choise = raw_input("Please choose [1-3] options: ")
+choise = int(choise)
+
+if choise == 1:
+	print datetime.now()
+elif choise == 2:
+	print "Starting backup"
+elif choise ==3:
+	print "Exiting programm"
+else:
+	print "Incorrect value entered, try again"		
+
