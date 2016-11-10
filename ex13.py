6483332b3b3937a2c85913594fb338cfe113eb9b
Katatohuk <katatohuk@gmail.com>
2016-11-10 15:29:40 +0200

    ex13.py

---
+++ ex13.py
@@ -0,0 +1,7 @@
+from sys import argvs
+script, first, second, third = argvs
+
+print "This script is called", script
+print "My first variable is: ", first
+print "My second variable is: ", second
+print "My third variable is: ", third
