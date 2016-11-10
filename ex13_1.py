469d3fccb03da73bb3f118c8fbb9a07a8d695700
Katatohuk <katatohuk@gmail.com>
2016-11-10 15:55:24 +0200

    d

---
+++ ex13_1.py
@@ -0,0 +1,11 @@
+from sys import argv
+
+script, first, second = argv
+
+print "Script name is: ", script
+print "The first variable is: ", first
+print "The second variable uis: ", second
+a = raw_input("Are u satisfied ?: ")
+print a
+b = raw_input("Please evaluate programm quiality with numbers from 1 to 5: ")
+print b
