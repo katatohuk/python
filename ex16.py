35470e08f06a8ab79335545f35fddc234b67b611
Katatohuk <katatohuk@gmail.com>
2016-11-15 16:56:49 +0200

    sss

--- ex16.py
+++ ex16.py
@@ -22,13 +22,16 @@ line3 = raw_input("Line 3: ")
 
 print "Now I'm goint write above to the file."
 
-target.write(line1)
-target.write("\n")
-target.write(line2)
-target.write("\n")
-target.write(line3)
-target.write("\n")
-
+#Lets write in 2 ways, using 6 lines or just 1
+#1
+#target.write(line1)
+#target.write("\n")
+#target.write(line2)
+#target.write("\n")
+#target.write(line3)
+#target.write("\n")
+#2
+target.write(line1 + '\n' + line2 + '\n' + line3)
 print "And finally lets close that file."
 
 target.close()
