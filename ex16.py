ed34910c0d7e6280ccacb44f9f9f949f1eef86a3
Katatohuk <katatohuk@gmail.com>
2016-11-15 15:19:43 +0200

    ex16.py added

---
+++ ex16.py
@@ -0,0 +1,34 @@
+from sys import argv
+
+script, filename = argv
+
+print "We're going to erase %r." % filename
+print "If u don't wanna do that press CTRL-C"
+print "If u DO wanna do that, press ENTER"
+
+raw_input('?')
+
+print "Opening the file..."
+target = open(filename, 'w')
+
+print "Truncating the file, goodbye"
+target.truncate()
+
+print "Now I'm going to ask you to provide 3 lines."
+
+line1 = raw_input("Line 1: ")
+line2 = raw_input("Line 2: ")
+line3 = raw_input("Line 3: ")
+
+print "Now I'm goint write above to the file."
+
+target.write(line1)
+target.write("\n")
+target.write(line2)
+target.write("\n")
+target.write(line3)
+target.write("\n")
+
+print "And finally lets close that file."
+
+target.close()
