0110b7a1166794cab8f730ec95a5888a38b34be0
Katatohuk <katatohuk@gmail.com>
2016-11-24 15:37:16 +0200

    n/a

--- ex19_1.py
+++ ex19_1.py
@@ -1,11 +1,10 @@
 #Lets write function to open file and then write some stuff in it.
 #This is the same was done in ex17_1.py but using a function
-from sys import argv, exit
-from os.path import exists
 
 def read_write(output_file, input_file):
 	indata = open(output_file).read()
 	print "Reading text from file %s" % output_file
+	print "The following text:\n%swill be copied" % indata
 	outdata = open(input_file, "w").write(indata)
 	print "Writing data to file %s" % input_file
 
