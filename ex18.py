01f448bf75c5c99d47e2d388dbac6ef3deb09257
Katatohuk <katatohuk@gmail.com>
2016-11-24 15:08:17 +0200

    ex18.py

---
+++ ex18.py
@@ -0,0 +1,21 @@
+# this one is like your scripts with argv
+def print_two(*args):
+	arg1, arg2 = args
+	print "arg1: %r, arg2: %r" % (arg1, arg2)
+
+# ok, that *args is actually pointless, we can just do this
+def print_two_again(arg1, arg2):
+	print "arg1: %r, arg2: %r" % (arg1, arg2)
+
+# this just takes 1 argument	
+def print_one(arg1):
+	print "arg1: %r" % arg1
+
+# this doesnt take any arguments
+def print_none():
+	print "I got nothing."
+
+print_two("Hi, ", "Vasya!")
+print_two_again("Hi, ", "Vasya!")
+print_one("Hi")
+print_none()		
