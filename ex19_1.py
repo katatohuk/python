d2523c7cae6508d10e043c6575c5389838240eac
Katatohuk <katatohuk@gmail.com>
2016-11-24 15:29:44 +0200

    ex19_1.py/ ex19 sample files

---
+++ ex19_1.py
@@ -0,0 +1,12 @@
+#Lets write function to open file and then write some stuff in it.
+#This is the same was done in ex17_1.py but using a function
+from sys import argv, exit
+from os.path import exists
+
+def read_write(output_file, input_file):
+	indata = open(output_file).read()
+	print "Reading text from file %s" % output_file
+	outdata = open(input_file, "w").write(indata)
+	print "Writing data to file %s" % input_file
+
+read_write("ex19_out", "ex19_in")
