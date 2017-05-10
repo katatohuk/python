2f0accfee6b5e61e115f29d2454a94fff39ee9c2
Katatohuk <katatohuk@gmail.com>
2017-05-10 17:27:57 +0300

    zzz

--- ex5.py
+++ ex5.py
@@ -1,18 +1,21 @@
-my_name = "Andrii"
-my_age = 31
-my_height = 193
-my_height_in = my_height * 0.39370
-my_weight = 93
-my_weight_lb = my_weight * 2.2046
-my_eyes = 'blue'
+my_name = 'Parkhomenko Andrii'
+my_age = 32
+my_height_cm = 192 # cm
+my_weight_kg = 95 # kg
+my_height_inch = my_height_cm / 2.5
+my_height_inch_rnd = round(my_height_inch,1)
+my_weight_pnd = my_weight_kg / 0.45
+my_weight_pnd_rnd = round(my_weight_pnd,1)
+my_eyes = 'Blue'
 my_teeth = 'white'
-my_hair = 'blond'
+my_hair = 'blonde'
 
-print "Lets talk about %s" % my_name
-print "He's %d santimeter (%d inches) tall" % (my_height, my_height_in)
-print "He's %d kgs (%d lbs) heavy" % (my_weight, my_weight_lb)
-print "Actually thtat's not too heavy :)"
-print "He's got %s eyes and %s hair" % (my_eyes, my_hair)
-print "His teeth are usually %s depenging on coffee" % my_teeth
+print(f"Lets talk about {my_name}.")
+print(f"He's {my_age} years old.")
+print(f"He's {my_height_cm}/{my_height_inch_rnd} centimiters/inches tall.\nHe's {my_weight_kg}/{my_weight_pnd_rnd} kilogramms/pounds heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the coffee and cigarettes.")
 
-print "If I add %d, %d, and %d I get %d" % (my_age, my_height, my_weight, my_age + my_height + my_weight)
+total = my_age + my_height_cm + my_weight_kg
+
+print(f"If I add {my_age}, {my_height_cm} and {my_weight_kg} I get {total}.")
