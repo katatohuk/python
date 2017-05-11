438e27c5b11b82c635dfd49896f3c2505e544919
Katatohuk <katatohuk@gmail.com>
2017-05-11 08:53:00 +0300

    zz

--- ex5.py
+++ ex5.py
@@ -2,17 +2,17 @@ my_name = 'Parkhomenko Andrii'
 my_age = 32
 my_height_cm = 192 # cm
 my_weight_kg = 95 # kg
-my_height_inch = my_height_cm / 2.5
-my_height_inch_rnd = round(my_height_inch,1)
-my_weight_pnd = my_weight_kg / 0.45
-my_weight_pnd_rnd = round(my_weight_pnd,1)
+#Now lets convert cm to inches and round it with round func
+my_height_inch = my_height_cm / round(2.5,1)
+##Now lets convert kg to pounds and round it with round func
+my_weight_pnd = my_weight_kg / round(0.45,1)
 my_eyes = 'Blue'
 my_teeth = 'white'
 my_hair = 'blonde'
 
 print(f"Lets talk about {my_name}.")
 print(f"He's {my_age} years old.")
-print(f"He's {my_height_cm}/{my_height_inch_rnd} centimiters/inches tall.\nHe's {my_weight_kg}/{my_weight_pnd_rnd} kilogramms/pounds heavy.")
+print(f"He's {my_height_cm}/{my_height_inch} centimiters/inches tall.\nHe's {my_weight_kg}/{my_weight_pnd} kilogramms/pounds heavy.")
 print(f"He's got {my_eyes} eyes and {my_hair} hair.")
 print(f"His teeth are usually {my_teeth} depending on the coffee and cigarettes.")
 
