e8fd0ad98bac80138d77ae180a9cda4b0434c96a
Katatohuk <katatohuk@gmail.com>
2017-05-11 11:09:08 +0300

    zz

--- ex8.py
+++ ex8.py
@@ -1,11 +1,11 @@
-formatter = "%r %r %r %r"
-print formatter % (1, 2, 3, 4)
-print formatter % ("one", "two", "three", "four")
-print formatter % (True, False, False, True)
-print formatter % (formatter, formatter, formatter, formatter)
-print formatter % (
- 				"I had this thing.",
-    			"That you could type up right.",
-    			"But it didn't sing.",
-    			"So I said goodnight."
-    			)
+formatter = "{} {} {} {}"
+print(formatter.format(1, 2, 3, 4))
+print(formatter.format('one', 'two', 'three', 'four'))
+print(formatter.format(True, False, False, True))
+print(formatter.format(formatter, formatter, formatter, formatter))
+print(formatter.format(
+		"Here should be some text",
+		"Bla bla bla",
+		"Hehehehehe",
+		"Coll, yeah ?"
+	))
