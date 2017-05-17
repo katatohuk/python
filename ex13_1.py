868c948723354e1df70d4f7f87013745fac17edb
Katatohuk <katatohuk@gmail.com>
2017-05-17 16:10:57 +0300

    ss

--- ex13_1.py
+++ ex13_1.py
@@ -2,10 +2,13 @@ from sys import argv
 
 script, first, second = argv
 
-print "Script name is: ", script
-print "The first variable is: ", first
-print "The second variable uis: ", second
-a = raw_input("Are u satisfied ?: ")
-print a
-b = raw_input("Please evaluate programm quiality with numbers from 1 to 5: ")
-print b
+print("Script name is: ", script)
+print("The first variable is: ", first)
+print("The second variable uis: ", second)
+a = input("Are u satisfied ? Type Y or N: ")
+if a == "Y":
+	print("So u'r satisfied :)")
+else:
+	print("So u'r not satisfied :( ")	
+b = input("Please evaluate programm quiality with numbers from 1 to 5: ")
+print(b)
