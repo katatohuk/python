86db2c8361118dafa6ac0e4dac05df91e287c7ab
katatohuk <katatohuk@gmail.com>
2017-05-17 19:31:15 +0300

    zz

--- ex14.py
+++ ex14.py
@@ -1,23 +1,21 @@
 from sys import argv
-script, user_name, age = argv
 
-promt = ':: '
+script, username = argv
+promt = '> '
 
-print "Hi %s, I'm the script %s" % (user_name, script)
-print "I'd like to ask u a few questions"
-print "Do you like me %s" % user_name
-likes = raw_input(promt)
+print(f"Hi {username}, I'm the {script} script.")
+print(f"I'd like to ask you a few questions.")
+print(f"Do you like me {username} ?")
+likes = input(promt)
 
-print "Where do you live %s?" % user_name
-lives = raw_input(promt)
+print(f"Where do you live {username} ?")
+lives = input(promt)
 
-print "What kind of computer do you have?"
-computer = raw_input(promt)
+print(f"What kind of computer do you have ?")
+comp = input(promt)
 
-print """
-Alright, so u said %r about liking me.
-You live in %s and I know u're %s years old.
-Hehe, dont be scared, I know smth about u already.
-Anyway I'm not sure where %s is.
-And you have a %s computer. Nice.
-""" % (likes, lives, age, lives, computer)
+print(f"""
+Alright, so you said you "{likes}" about liking me.
+You line in {lives}. Not sure where that is.
+And you have a {comp} computer. Cool ! 
+""")
