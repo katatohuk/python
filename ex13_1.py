5f85ba9e0e866aefbe0a5ee007e72bd30e2564fc
Katatohuk <katatohuk@gmail.com>
2017-08-15 16:18:47 +0300

    ==

--- ex13_1.py
+++ ex13_1.py
@@ -6,8 +6,8 @@ print("Script name is: ", script)
 print("The first variable is: ", first)
 print("The second variable uis: ", second)
 a = input("Are u satisfied ? Type Y or N: ")
-if a == "Y":
-	print("So u'r satisfied :)")
+if a == "Y" or a == "y":
+	print("Glad to hear you really like it!")
 else:
 	print("So u'r not satisfied :( ")	
 b = input("Please evaluate programm quiality with numbers from 1 to 5: ")
