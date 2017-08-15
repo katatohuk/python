0535604d9e30f6579bbb2ab01748333b65e5495d
Katatohuk <katatohuk@gmail.com>
2017-08-15 16:15:33 +0300

    -

--- ex13.py
+++ ex13.py
@@ -1,3 +1,11 @@
+#usage: ex13.py 1 2 3
+#output:
+#Andrey@/git/python| python3 ex13.py 1 2 3
+#The script is called:  ex13.py
+#1st variable is:  1
+#2nd variable is:  2
+#3rd varialbe is:  3
+
 from sys import argv
 script, first, second, third = argv
 
