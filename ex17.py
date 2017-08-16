edbedaa67daf3bbf034f04e2b4b49dd5f33536a2
Katatohuk <katatohuk@gmail.com>
2017-08-16 12:39:37 +0300

    --

--- ex17.py
+++ ex17.py
@@ -3,20 +3,23 @@ from os.path import exists
 
 script, from_file, to_file = argv
 
-print "Copyin from %s to %s file." % (from_file, to_file)
+print(f"Copying data from {from_file} to {to_file}")
 
-in_file = open(from_file)
-indata = in_file.read()
+#open file and read it in one line, instead of separate commands to open and then read
+in_file = open(from_file).read()
 
-print "The input file is %s bytes long" % len(indata)
+print(f"Here is content of {from_file} file: " + "\n", in_file)
 
-print "Does the output filr exist? %r" % exists(to_file)
-print "Ready, hit ENTER to continue, CTRL-C to abort"
-raw_input()
+print(f"Lets check if sorce file isnt empty")
+if len(in_file) == 0:
+	exit("Source file is 0 bytes, so its empty. Please add some data")
+else:
+	print("Source file is okay")
 
-out_file = open(to_file, 'w')
-out_file.write(indata)
+out_file = open(to_file, 'w').write(in_file)
+out_file_read = open(to_file).read()
 
-print "Allright, all done."
-out_file.close()
-in_file.close()
+print(f"Here is content of {to_file}: " + "\n", out_file_read)
+
+
+
