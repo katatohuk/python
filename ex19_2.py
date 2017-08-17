3fd0609266858e522e00da735849e996031e9fe0
Katatohuk <katatohuk@gmail.com>
2017-08-17 15:17:22 +0300

    --

--- ex19_2.py
+++ ex19_2.py
@@ -1,8 +1,13 @@
-def read_write(output_file, input_file):
-	indata = open(output_file).read()
-	print "Reading text from file %s" % output_file
-	print "The following text:\n%swill be copied" % indata
-	outdata = open(input_file, "w").write(indata)
-	print "Writing data to file %s" % input_file
+def cheese_and_crackers(cheese_count,box_of_crackers,sum):
+	print(f"You have {cheese_count} cheeses!")
+	print(f"You have {box_of_crackers} boxes of crackers!")
+	print(f"Totally you have {sum} cheese and crackers!\n")
 
-read_write("ex19_out", "ex19_in")
+print("Buddy, lets count your cheese and crackers!")
+amount_of_cheese = input("Cheese quantity: ")
+amount_of_crackers = input("Cracker boxes quantity: ")
+sum_amount = int(amount_of_crackers) + int(amount_of_cheese)
+
+cheese_and_crackers(amount_of_cheese, amount_of_crackers, sum_amount)
+
+
