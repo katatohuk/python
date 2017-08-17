60cb4f5281439c655ac90427038d045ed5605f9e
Katatohuk <katatohuk@gmail.com>
2017-08-17 13:27:54 +0300

    --

--- ex19.py
+++ ex19.py
@@ -1,21 +1,17 @@
-def cheese_and_crackers(cheese_count, boxes_of_crackers):
-	print "You have %d cheeses." % cheese_count
-	print "You have %d boxes of ckackers." % boxes_of_crackers
-	print "Man, that's enough for a party!"
-	print "Get a blanket!\n"
+def cheese_and_crackers(cheese_count,box_of_crackers):
+	print(f"You have {cheese_count} cheeses!")
+	print(f"You have {box_of_crackers} boxes of crackers!\n")
 
-print "#We can just give the functions numbers directly."
+print("We cant give the function numbers directly:")
 cheese_and_crackers(20, 30)
 
-print "#Or we can use variables from our script."
+print("or use variables from our script:")
 amount_of_cheese = 10
 amount_of_crackers = 50
-
 cheese_and_crackers(amount_of_cheese, amount_of_crackers)
 
-print "#We can even do some math inside."
-cheese_and_crackers(10 + 20, 30 + 50)
+print("We can eve do some math inside:")
+cheese_and_crackers(10 + 20, 5 + 6)
 
-print "#And we can combine the two, variables and math"
-cheese_and_crackers(amount_of_cheese + 10, amount_of_crackers + 3)
-
+print("And we can combine variables and math:")
+cheese_and_crackers(amount_of_cheese + 100, amount_of_crackers + 1000)
