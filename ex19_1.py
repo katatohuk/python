3f10b38742aef01addda64c9d8f90ccd1c6c9178
Katatohuk <katatohuk@gmail.com>
2017-08-17 13:35:18 +0300

    --

--- ex19_1.py
+++ ex19_1.py
@@ -3,9 +3,9 @@
 
 def read_write(output_file, input_file):
 	indata = open(output_file).read()
-	print "Reading text from file %s" % output_file
-	print "The following text:\n%swill be copied" % indata
+	print(f"Reading text from file {output_file}")
+	print(f"The following text will be copied :\n", indata)
 	outdata = open(input_file, "w").write(indata)
-	print "Writing data to file %s" % input_file
+	print(f"Writing data to file {input_file}")
 
 read_write("ex19_out", "ex19_in")
