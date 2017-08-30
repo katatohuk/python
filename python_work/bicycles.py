a7ca0743907e7361c554834644d5f67d720abf0d
Katatohuk <katatohuk@gmail.com>
2017-08-30 13:00:23 +0300

    rr

---
+++ python_work/bicycles.py
@@ -0,0 +1,7 @@
+bicycles = ['aist', 'ukraina', 'orlenok']
+print(bicycles[1].title())
+
+#Show last element in the list -1
+print(bicycles[-1].title())
+
+print("My first bicycle was " + bicycles[0].title() + ".")
---
+++ python_work/cars.py
@@ -0,0 +1,6 @@
+cars = ['audi', 'vw', 'seat', 'bwm']
+print(cars)
+
+#Lets replace first element in the list
+cars[0] = 'mercedes'
+print(cars)
