c10edcaae2cf4209cdb6e28f92ae0ad910f54510
Katatohuk <katatohuk@gmail.com>
2017-08-30 13:08:29 +0300

    --

--- python_work/cars.py
+++ python_work/cars.py
@@ -4,3 +4,23 @@ print(cars)
 #Lets replace first element in the list
 cars[0] = 'mercedes'
 print(cars)
+
+#Appen will add new element to the end of list
+cars.append('hyundai')
+print(cars)
+
+#Lets create empty list and later add items
+cars = []
+print(cars)
+cars.append('audi')
+cars.append('honda')
+cars.append('vw')
+print(cars)
+
+#insert will add element to the list in particular position
+cars.insert(0, 'kia')
+print(cars)
+
+#del will delete particular item from the list, using position index
+del cars[0]
+print(cars)
