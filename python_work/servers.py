4b517ec5d75e68d01ee033225625a8dc47b037d3
Katatohuk <katatohuk@gmail.com>
2017-08-31 14:05:54 +0300

    --

--- python_work/servers.py
+++ python_work/servers.py
@@ -1,3 +1,14 @@
+#Loop the list with some values
 servers = ['server_a', 'server_b', 'server_c', 'server_d']
 for server in servers:
 	print(server)
+
+
+ 
+#Loop range of numbers
+for x in range(1,10):
+	print(x)
+
+#Create numeric list
+numbers = list(range(1,11))
+print(numbers)
