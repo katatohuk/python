e50550dae418073c0513c28c0535e778c509cc91
Katatohuk <katatohuk@gmail.com>
2017-10-04 17:21:13 +0300

    --

---
+++ python_work/chapter6/alien.py
@@ -0,0 +1,12 @@
+#Set dictionary and ready keys from it
+alien_0 = {'color': 'green', 'points': 5}
+#print(alien_0)
+print(alien_0['color'])
+print(alien_0['points'])
+
+#Lets add new keys/values to our dic
+alien_0['x_position'] = 0
+alien_0['y_position'] = 25
+
+print(alien_0)
+
