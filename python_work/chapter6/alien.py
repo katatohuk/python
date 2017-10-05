c8b2453e15b761eec26905830cfacd7d97398b45
Katatohuk <katatohuk@gmail.com>
2017-10-05 12:29:32 +0300

    -

--- python_work/chapter6/alien.py
+++ python_work/chapter6/alien.py
@@ -11,8 +11,13 @@ print(alien_0)
 
 #Lets create empty dic
 alien_0 = {}
-
+#And add new items
 alien_0['color'] = 'green'
 alien_0['points'] = 5
 print(alien_0)
+
+#Lets modify existing value
+alien_0 = {'color': 'yellow'}
+#And check that was modified from green to yellow
+print(alien_0['color'])
 
