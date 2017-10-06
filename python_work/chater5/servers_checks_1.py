c2e921e30e33d39a5e78c2dd9b3b1b326dd8e8f7
Katatohuk <katatohuk@gmail.com>
2017-10-06 15:46:11 +0300

    --

--- python_work/chater5/servers_checks_1.py
+++ python_work/chater5/servers_checks_1.py
@@ -2,38 +2,10 @@
 # Env server list is hardcoded in this scrip, used dictionary "envs" here
 #  
 
-#script, input_server = argv
-#print(input_server + "\n")
-
-# Can be done in this way as well
 f = open("envs.txt").read().splitlines()
-#f = open("list.txt").readlines()
-#print(f)
-#envs = list(f)
-#print(envs)
 in_file = open("input.txt").read().splitlines()
 input_list = list(in_file)
-#print(input_list)
 env_file = open("envs.txt").read()
-#print(env_file)
-
-#env = {}
-#print(env_file.split())
-
-
-#for line in env_file:
-    	#print(a,b)
-	#env['a'] = b
-
-#print(env)
-#envs = dict(key, value.strip())
-
-#print(envs)
-
-
-
-#if input_server in envs:
-#	print("Server is in the list")
 
 envs = {
 'server1': 'IDP : uat1',
@@ -56,41 +28,8 @@ envs = {
 'server18': 'MDF : uatx',
 }
 
-#print("\n")
-#print(envs)
-#idp = sorted(idp.items(), key=lambda item: item[0])
-#print(idp)
-
-
-#envs = [idp, mdf]
-#print(envs)
-
 for key, value in envs.items():
 	#print("\n" + key)
 	#print(value)
 	if key in input_list:
     		print(value + " : " + key)
-
-#for key, value in mdf.items():
-#	#print("\n" + key)
-#	#print(value)
-#	if key in input_list:
-#    		print("MDF : " + key + " : " + value)
-
-
-#for key, value in mdf.items():
-#	print("\n" + key)
-#	print(value)
-#	if value in input_list:
-#    		print(value)
-#    		print("MDF : " + key)
-
-
-#for value in input_list:
-#	if value in idp:
-#    		print("IDP : " + key)
-
-#for key, value in mdf.items():
-#	if value in input_list:
-#    		print("MDF : " + key)
-
