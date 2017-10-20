4243fcafc8449f0b89ed2dbdc697157af2063dea
Katatohuk <katatohuk@gmail.com>
2017-10-20 10:58:00 +0300

    --

---
+++ python_work/chater5/servers_checks_3.py
@@ -0,0 +1,37 @@
+# Chek servers by the list
+# Input list is taken from file "input.txt"
+# Apps/envs/hosts mapping is in the envs1.txt
+#  
+
+in_file = open("input.txt").read().splitlines()
+input_list = list(in_file)
+print(input_list)
+#print(input_list)
+env_file = open("envs1.txt").read().splitlines()
+#print(env_file)
+
+
+#splits = env_file.split(',')
+#print(splits)
+#print(len(splits))
+
+envs = {}
+for line in env_file:
+	#print(line.split(',',1))
+	a, b = line.split(',',1)
+	envs[a] = b
+	#print(envs)
+
+#print("\n")
+#print(envs)
+
+print("\n")
+#print(sorted(envs.keys())
+#print(envs['server1'])
+
+for key, value in sorted(envs.items()):
+	#print("\n" + key)
+	#print(value)
+	if key in input_list:
+    		print(value + " : " + key)
+
