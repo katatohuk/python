3dea2a2f1563f6ad49b39cf4748783cc9a56e211
Andrii Parkhomenko <ADPK@simcorp.com>
2018-05-30 14:31:53 +0300

    s

--- powershell/check_os_version.ps1
+++ powershell/check_os_version.ps1
@@ -1,1 +1,3 @@
+# put a file with server list somewhere as in below example
+$servers = Get-Content \\dk01sn008\Test\Temp\RMNM\ex64.txt
 Get-CimInstance Win32_OperatingSystem -ComputerName $servers | Select-Object CSName, Caption | Sort-Object -Property CSName
