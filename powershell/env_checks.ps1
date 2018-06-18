27f8d5843e09c9752530358ae63197af2278be2c
Andrii Parkhomenko <ADPK@simcorp.com>
2018-06-18 17:47:40 +0300

    --

--- powershell/env_checks.ps1
+++ powershell/env_checks.ps1
@@ -1,6 +1,6 @@
 ########################################################################
 ## Sanity checks for Execution servers
-## 1) Check D: drive availability and space
+## 1) Check D: drive availability and size
 ## 2) Check regional settings
 ## 3) Check pagefile settings
 ## 4) Check TWS agent installed, registered on Tivoli server and running
@@ -25,19 +25,17 @@ Write-host -Backgroundcolor 'Black' ('###########################')
 #Get Windows version
 function os_ver { 
                 param ([array]$servers)
-                Get-CimInstance Win32_OperatingSystem -ComputerName $servers | Select-Object CSName, Caption | Sort-Object -Property CSName | where {$_.Caption -in "Microsoft Windows Server 2012 R2 Standard", "Microsoft Windows Server 2016 Standard"}
+                Get-CimInstance Win32_OperatingSystem -ComputerName $server | Select-Object CSName, Caption | Sort-Object -Property CSName | where {$_.Caption -in "Microsoft Windows Server 2012 R2 Standard", "Microsoft Windows Server 2016 Standard"}
                 }
 
 #Check disk size by drive letter
 function disk {[cmdletbinding()]
-               param ([string]$drive,[array]$server) 
+               param ([string]$drive) 
                #Write-Host ('-----------------------------------------------------')
                #Write-Host ('Server: ' + $server.ToUpper())
                $check = Invoke-Command -ComputerName $server -ArgumentList $drive -ScriptBlock {param ($drive) Get-WmiObject Win32_logicaldisk | select -Property DeviceId, @{Name='GB'; Expression={[math]::round($_.size/1GB, 2)}} | where -Property DeviceId -eq $drive} 
                $check  
                        }
-
-#disk -drive D: -server DK01SV8140 -Debug
 
 
 #Pagefile checks for Windows 2012
@@ -47,8 +45,8 @@ function pagefile_12 {
                     #Write-Host ('-----------------------------------------------------')        
                     #Write-Host ('Server: ' + $servers)
                     Write-Host -NoNewline ('OS version: ') 
-                    Get-CimInstance Win32_OperatingSystem -ComputerName $servers| Select-Object -ExpandProperty Caption
-                    $win12_check = Invoke-Command -ComputerName $servers -ScriptBlock {Get-WmiObject -Class Win32_ComputerSystem | fl AutomaticManagedPagefile} | Out-String
+                    Get-CimInstance Win32_OperatingSystem -ComputerName $server | Select-Object -ExpandProperty Caption
+                    $win12_check = Invoke-Command -ComputerName $server -ScriptBlock {Get-WmiObject -Class Win32_ComputerSystem | fl AutomaticManagedPagefile} | Out-String
                     $win12_check = $win12_check.Trim()
                     $win12_check
                     Write-Host ('')
@@ -74,12 +72,12 @@ function pagefile_16 {
                     #Write-Host ('-----------------------------------------------------')
                     #Write-Host ('Server: ' + $servers)
                     Write-Host -NoNewline ('OS version: ') 
-                    Get-CimInstance Win32_OperatingSystem -ComputerName $servers | Select-Object -ExpandProperty Caption
-                    $win16_check_1 = Invoke-Command -ComputerName $servers -ScriptBlock {Get-WmiObject -Class Win32_ComputerSystem | fl AutomaticManagedPagefile} | Out-String
+                    Get-CimInstance Win32_OperatingSystem -ComputerName $server | Select-Object -ExpandProperty Caption
+                    $win16_check_1 = Invoke-Command -ComputerName $server -ScriptBlock {Get-WmiObject -Class Win32_ComputerSystem | fl AutomaticManagedPagefile} | Out-String
                     $win16_check_1 = $win16_check_1.Trim()
                     $win16_check_1
                     #Checking pagefile allocated to 65Gb
-                    $win16_check_2 = Invoke-Command -cn $servers -ScriptBlock {Get-ItemProperty -Path Registry::'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management' | Select-Object -ExpandProperty PagingFiles}
+                    $win16_check_2 = Invoke-Command -cn $server -ScriptBlock {Get-ItemProperty -Path Registry::'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management' | Select-Object -ExpandProperty PagingFiles}
                     $win16_check_2 = $win16_check_2.Trim()
                     $win16_check_2
                     Write-Host ('')
@@ -123,14 +121,24 @@ function net_framework {
 Invoke-Command -cn $server -ScriptBlock {Get-ItemPropertyValue 'HKLM:SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full\' -Name Release}
 }
 
+# Check software is installed
+function is-installed ($program) {
+                                    Invoke-Command -ComputerName $server -ScriptBlock {$x86 = ((Get-ChildItem "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall") |
+                                        Where-Object { $_.GetValue( "DisplayName" ) -like "*$program*" }).Length -gt 0;
+                                    
+                                         $x64 = ((Get-ChildItem "HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall") |
+                                         Where-Object { $_.GetValue( "DisplayName" ) -like "*$program*" }).Length -gt 0;
+
+                                            $x86 -or $x64; }
+}
 
 
 # Run loop for all servers in array
 foreach($server in $servers)
 {
 Write-Host ('')
-Write-Host ('Checking Pagefile status')
 Write-Host ('-----------------------------------------------------')
+Write-Host -ForegroundColor darkYellow ('Checking Pagefile status')
 $os_ver = os_ver $server
      if($os_ver.Caption -match 'Microsoft Windows Server 2012 R2 Standard')
         {
@@ -141,9 +149,9 @@ $os_ver = os_ver $server
                     pagefile_16 $os_ver.CSName
                 }
 Write-Host ('')
-Write-Host ('Checking drive D:\ status')
 Write-Host ('-----------------------------------------------------')
-$check_d = disk D: $server
+Write-Host -ForegroundColor darkYellow ('# Checking drive D:\ status')
+$check_d = disk D:
      if($check_d.GB -gt 14)
         {
             Write-Host -NoNewline ('Drive ' + $check_d.DeviceID + ' is ')
@@ -152,8 +160,8 @@ $check_d = disk D: $server
             else {Write-Host -NoNewline ('Drive ' + $check_d.DeviceID + ' is')
                   Write-host -ForegroundColor Red ( ' NOT ok')}
 Write-Host ('')
-Write-Host ('Checking locale')
 Write-Host ('-----------------------------------------------------')
+Write-Host -ForegroundColor darkYellow ('Checking locale')
 $locale = invoke-Command -cn $server -ScriptBlock {get-culture } | select -ExpandProperty name
     if($locale -eq 'da-DK')
        {
@@ -164,9 +172,14 @@ $locale = invoke-Command -cn $server -ScriptBlock {get-culture } | select -Expan
             Write-Host -NoNewline ('Regional settings:')
             Write-host -ForegroundColor Red ( 'NOT ok')}
 Write-Host ('')
-Write-Host ('Checking .NET Framework version installed')
 Write-Host ('-----------------------------------------------------')
+Write-Host -ForegroundColor darkYellow ('Checking .NET Framework version installed')
 $checkfx = net_framework $server
-     $dotNet4Builds[$checkfx]             
+     $dotNet4Builds[$checkfx]   
+Write-Host ('')
+Write-Host ('-----------------------------------------------------')
+Write-Host -ForegroundColor darkYellow ('Checking IBM Websphere MQ is  installed')
+$checkmq = is-installed IBM Websphere MQ
+$checkmq         
 }
 
